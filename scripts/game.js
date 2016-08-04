09f0bc96a28399fd90a2560bac6998fc3fb0b767
TeodSt <teod_stoyanova@abv.bg>
2016-08-05 00:21:42 +0300

    Added images to the bricks

--- scripts/game.js
+++ scripts/game.js
@@ -5,6 +5,7 @@ function createGame(canvasSelector) {
         canvasWidth = canvas.width,
         ball = generateBall(),
         bricks = [],
+        bricksImagesPaths = ["images/brick.png", "images/purple-brick.png", "images/yellow-brick.png", "images/green-brick.png", "images/pink-brick.png"],
         numberOfBricksInRow = 8,
         numberOfBricksInCol = 5,
         padHeight = 10,
@@ -26,9 +27,7 @@ function createGame(canvasSelector) {
                 isMoving = true;
             }
         }, false);
-        generateBricks();
         drawPad();
-        drawBricks();
     }
 
 
@@ -77,17 +76,19 @@ function createGame(canvasSelector) {
     }
 
     function drawBricks() {
-        debugger;
-        var image = new Image();
-        image.src = "images/brick.png";
-        for (var i = 0; i < bricks.length; i++) {
-            var currentBrick = bricks[i];
-            context.beginPath();
-            context.rect(currentBrick.x, currentBrick.y, currentBrick.width, currentBrick.height);
-            //context.drawImage(image, currentBrick.x, currentBrick.y, currentBrick.width, currentBrick.height);               
-            context.fill();
-            context.closePath();
-        }
+        generateBricks();
+        var imgs = [];
+        for (var j = 0; j < bricks.length; j++) { 
+            var img = new Image();
+            img.src = bricks[j].image;
+            imgs.push(img);             
+         }
+
+         $.each(imgs, function(key, value ) {
+             value.onload = function(){
+                context.drawImage(value, bricks[key].x, bricks[key].y, bricks[key].width, bricks[key].height );
+            };
+        });       
     }
 
     function createBall(x, y, radius, speed, direction) {
@@ -109,11 +110,13 @@ function createGame(canvasSelector) {
     }
 
     function createBrick(x, y) {
+        var randomIndex = (Math.random() * (bricksImagesPaths.length - 0) + 0) | 0;
         var brick = {
             x: x,
             y: y,
             width: 50,
-            height: 20
+            height: 20, 
+            image: bricksImagesPaths[randomIndex]
         };
 
         return brick;
@@ -138,6 +141,7 @@ function createGame(canvasSelector) {
 
     return {
         "start": function() {
+            drawBricks();
             gameLoop();
         }
     };
